14836dca54759812ffe6022e5af45cd87718640d
Uzair-khane <uzairkhanar123@gmail.com>
2025-12-19 22:43:36 +0500

    add facicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,13 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
  compatibilityDate: '2025-07-15',
  devtools: { enabled: true },
-
+  app: {
+    head: {
+      link: [
+        { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+      ],
+    },
+  },
  css: [
     '~/assets/css/main.css' 
   ],
