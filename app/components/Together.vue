ca3aa665fcc08f440da1e9824dcaeef15cd48206
Uzair-khane <uzairkhanar123@gmail.com>
2025-12-19 15:58:46 +0500

    add contect us page and also add responsive style

--- app/components/Together.vue
+++ app/components/Together.vue
@@ -3,7 +3,7 @@
     <h2 class="text-3xl md:text-4xl font-extrabold text-white mb-6">
       Making a Difference Together
     </h2>
-    <p class="text-white text-lg md:text-xl leading-relaxed mx-28">
+    <p class="text-white text-lg md:text-xl leading-relaxed mx-8">
   We are inspired by the dream of a clean and safe environment, reducing waste and promoting sustainable practices.
 Take one simple step today to protect our planet and ensure a greener future.
 Every small action matters, and together we can create meaningful change.
